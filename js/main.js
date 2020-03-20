fc213416577ceb2918ad005b52033f928ba130b3
mj0se <mariajose1907@gmail.com>
2020-03-20 10:13:40 +0100

    changes on api results

--- js/main.js
+++ js/main.js
@@ -3,62 +3,32 @@ const btn = document.querySelector('.js-btn');
 const inputText = document.querySelector('.js-input');
 const inputTextValue = inputText.value;
 let show = document.querySelector('.js-show');
-let series = [];
-
+let saveShow = [];
 
 // solicitar datos a la api solo de lo que queremos imprimir, imagen y nombre :
 
-function getInfoShows() {
-
-    fetch(`http://api.tvmaze.com/shows?=${inputTextValue}`)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-
-            for (let index = 0; index < data.length; index++) {
-
-                series.push({
-                    image: data[index].image.medium,
-                    name: data[index].name
-                });
-                // console.log(series[index].name);
-                // console.log(series[index].image);
-            }
-            paintShows();
+function getInfoApiShow(ev) {
+  ev.preventDefault();
+  const inputTextValue = inputText.value;
+  fetch(`http://api.tvmaze.com/search/shows?q=${inputTextValue}`)
+    .then(response => response.json())
+    .then(data => {
+      for (let index = 0; index < data.length; index++) {
+        saveShow.push({
+          name: data[index].show.name,
+          image: data[index].show.image.medium
         });
+      }
+      getResultsApi(saveShow);
+    });
 }
 
-
-// pintar la serie que buscamos
-
-
-function paintShows() {
-
-    let htmlCodeShow = `<ul class="show__section">`
-
-    for (const show of series) {
-        htmlCodeShow += `<li>`
-        htmlCodeShow += `<h3>${show.name} </h3>`
-        htmlCodeShow += `<img src="${show.image}" alt="image-of-show">`
-        htmlCodeShow += `</li>`
-
-        console.log(show.name);
-        console.log(show.image);
-    }
-
-    htmlCodeShow = `</ul>`;
-    show.innerHTML = htmlCodeShow;
-
+function getResultsApi(DataShow) {
+  console.log(DataShow);
 }
 
+// pintar html de los datos buscados por el usuario
 
 // evento click del botón buscar
-const clickButtonSearch = () => {
-    console.log('Me han clickado');
 
-}
-
-getInfoShows();
-
-btn.addEventListener('click', clickButtonSearch);
+btn.addEventListener('click', getInfoApiShow);
