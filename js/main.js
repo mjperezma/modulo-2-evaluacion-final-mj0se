32ef27bd090831aa2a1430354efb7bb3dc8959ce
mj0se <mariajose1907@gmail.com>
2020-03-21 14:32:09 +0100

    add localstorage and favourite section

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ const inputText = document.querySelector('.js-input');
 const inputTextValue = inputText.value;
 let show = document.querySelector('.js-show');
 let saveShow = [];
+let favouriteShows = [];
 
 // solicitar datos a la api solo de lo que queremos imprimir, imagen, nombre e id :
 
@@ -13,8 +14,8 @@ function getInfoApiShow(ev) {
   fetch(`http://api.tvmaze.com/search/shows?q=${inputTextValue}`)
     .then(response => response.json())
     .then(data => {
+      saveShow.splice(0);
       for (let index = 0; index < data.length; index++) {
-        saveShow.splice();
         if (data[index].show.image === null) {
           saveShow.push({
             name: data[index].show.name,
@@ -28,9 +29,7 @@ function getInfoApiShow(ev) {
             id: data[index].show.id
           })
         };
-
       }
-
       paintResultShow();
     });
 }
@@ -42,17 +41,90 @@ function paintResultShow() {
   let htmlCodeShow = '';
 
   for (const show of saveShow) {
-    htmlCodeShow += `<li id="${show.id}" class="card__show">`;
-    htmlCodeShow += `<h3 class="card__show__title"> ${show.name}</h3>`;
-    htmlCodeShow += `<img class="img__card" src="${show.image}">`;
-    htmlCodeShow += `</li>`;
-    console.log(show.name);
-    console.log(show.image);
-  }
 
+    htmlCodeShow += `<li class="list" class="card__show">`;
+    htmlCodeShow += `<h3 class="card__show__title"> ${show.name}</h3>`;
+    htmlCodeShow += `<img class="card__show__img" src="${show.image}">`;
+    htmlCodeShow += `<button class="js-button js-id"  data-id="${show.id}" >Add to favourites</button>`
+    htmlCodeShow += `</li>`;
+
+  }
   show.innerHTML = htmlCodeShow;
+  lintenFavouriteShow();
 }
 
+
+// elegir serie favorita
+
+
+function lintenFavouriteShow() {
+
+  const btnfavourite = document.querySelectorAll('.js-id');
+  for (const btn of btnfavourite) {
+    btn.addEventListener('click', addFavouriteShow);
+  }
+}
+
+
+function addFavouriteShow(ev) {
+  const clickedIdShow = ev.target.dataset.id;
+  let foundFavouriteShow;
+  for (let show of saveShow) {
+    if (show.id === parseInt(clickedIdShow)) {
+
+      foundFavouriteShow = show;
+    }
+  }
+
+
+  favouriteShows.push({
+    id: foundFavouriteShow.id,
+    name: foundFavouriteShow.name,
+    image: foundFavouriteShow.image
+  })
+
+  setInLocalStorage();
+  paintFavouritesShows();
+}
+
+
+// función para pintar las series favoritas
+
+const ulFavourite = document.querySelector('.card__favourites');
+
+function paintFavouritesShows() {
+
+  let htmlCodeFavouriteShow = '';
+
+  for (const favourite of favouriteShows) {
+
+    htmlCodeFavouriteShow += `<li class="list" class="card__show">`;
+    htmlCodeFavouriteShow += `<h3 class="card__show__title"> ${favourite.name}</h3>`;
+    htmlCodeFavouriteShow += `<img class="card__show__img" src="${favourite.image}">`;
+    htmlCodeFavouriteShow += `</li>`;
+
+  }
+  ulFavourite.innerHTML = htmlCodeFavouriteShow;
+  setInLocalStorage();
+  lintenFavouriteShow();
+}
+
+// funcion para guardar favoritas en localstorage
+function getFromLocalStorage() {
+  const localStorageShow = localStorage.getItem('favourite');
+  if (localStorageShow !== null) {
+    favouriteShows = JSON.parse(localStorageShow);
+    paintFavouritesShows();
+  }
+}
+
+function setInLocalStorage() {
+  const strinfigyShow = JSON.stringify(favouriteShows);
+  localStorage.setItem('favourite', strinfigyShow);
+
+}
 // evento click del botón buscar
 
 btn.addEventListener('click', getInfoApiShow);
+
+getFromLocalStorage();
