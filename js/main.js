f4d955549c9fc7650281cfebb0bd7c2457a834c3
mj0se <mariajose1907@gmail.com>
2020-03-22 13:35:46 +0100

    add css new styles, clean and add only one add

--- js/main.js
+++ js/main.js
@@ -21,111 +21,119 @@ function getInfoApiShow(ev) {
           saveShow.push({
             name: data[index].show.name,
             id: data[index].show.id,
-            image: "https://via.placeholder.com/210x295/ffffff/666666/?text=TV"
-          })
+            image: 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV'
+          });
         } else {
           saveShow.push({
             name: data[index].show.name,
             image: data[index].show.image.medium,
             id: data[index].show.id
-          })
-        };
+          });
+        }
       }
       paintResultShow();
     });
 }
 
+// creo el código html de los datos buscados por el usuario
 
-// pintar html de los datos buscados por el usuario
+function getHtmlCodeResultShow(show) {
+  let htmlCode = '';
+  htmlCode += `<li class="card__show js-card-show" id="${show.id}">`;
+  htmlCode += `<h3 class="card__show__title"> ${show.name}</h3>`;
+  htmlCode += `<img class="card__show__img" src="${show.image}">`;
+  htmlCode += `<button class="js-button js-id"  data-id="${show.id}" >Add to favourites</button>`;
+  htmlCode += `</li>`;
+  return htmlCode;
+}
+
+// pinto el resultado del código html
 
 function paintResultShow() {
   let htmlCodeShow = '';
-
   for (const show of saveShow) {
-
-    htmlCodeShow += `<li class="card__show js-card-show" id="${show.id}">`;
-    htmlCodeShow += `<h3 class="card__show__title"> ${show.name}</h3>`;
-    htmlCodeShow += `<img class="card__show__img" src="${show.image}">`;
-    htmlCodeShow += `<button class="js-button js-id"  data-id="${show.id}" >Add to favourites</button>`
-    htmlCodeShow += `</li>`;
-
+    htmlCodeShow += getHtmlCodeResultShow(show);
   }
   show.innerHTML = htmlCodeShow;
   lintenFavouriteShow();
 }
 
-
 // escuchar serie favorita
 
-
 function lintenFavouriteShow() {
-
   const btnfavourite = document.querySelectorAll('.js-id');
   for (const btn of btnfavourite) {
     btn.addEventListener('click', addFavouriteShow);
-
   }
-
 }
 
-// añadir serie favorita 
+// añadir serie favorita
 
 function addFavouriteShow(ev) {
+  // obtener el id de la serie clickada
   let clickedIdShow = ev.target.dataset.id;
-  let foundFavouriteShow;
-  for (let show of saveShow) {
-    if (show.id === parseInt(clickedIdShow)) {
-      foundFavouriteShow = show;
+  let foundItemFavouriteShow;
+  for (let item of favouriteShows) {
+    if (item.id === parseInt(clickedIdShow)) {
+      foundItemFavouriteShow = item;
     }
   }
-
-  function addColorToFavorites() {
-    const liIds = document.querySelectorAll('.js-card-show');
-    for (const liId of liIds) {
-      if (liId.id === clickedIdShow) {
-
-        liId.classList.add('card__show__fav');
-
+  if (foundItemFavouriteShow === undefined) {
+    // buscar el producto clickado
+    let foundFavouriteShow;
+    for (let show of saveShow) {
+      if (show.id === parseInt(clickedIdShow)) {
+        foundFavouriteShow = show;
       }
     }
+    // añadir color a series favoritas
+    function addColorToFavorites() {
+      const listFavIds = document.querySelectorAll('.js-card-show');
+      for (const liFav of listFavIds) {
+        if (liFav.id === clickedIdShow) {
+          liFav.classList.add('card__show__fav');
+        }
+      }
+    }
+    addColorToFavorites();
 
+    // añadirlo a favoritos
+    favouriteShows.push({
+      id: foundFavouriteShow.id,
+      name: foundFavouriteShow.name,
+      image: foundFavouriteShow.image
+    });
   }
-
-  addColorToFavorites();
-  favouriteShows.push({
-    id: foundFavouriteShow.id,
-    name: foundFavouriteShow.name,
-    image: foundFavouriteShow.image
-
-  })
   setInLocalStorage();
-  paintFavouritesShows();
+  paintCodeFavouriteShow();
 }
 
-
-// función para pintar en html las series favoritas
+// función para crear html las series favoritas
 
 const ulFavourite = document.querySelector('.card__favourites');
 
-function paintFavouritesShows() {
+function getHtmlCodeFavShow(favourite) {
+  let htmlCodeFav = '';
 
-  let htmlCodeFavouriteShow = '';
+  htmlCodeFav += `<li class="list" class="card__show" id="${favourite.id}">`;
+  htmlCodeFav += `<h3 class="card__show__title"> ${favourite.name}</h3>`;
+  htmlCodeFav += `<img class="card__show__img" src="${favourite.image}">`;
+  htmlCodeFav += `</li>`;
+  htmlCodeFav += `<button class="js-button js-id-remove"  data-id="${favourite.id}" >Delete favourites</button>`;
 
-  for (const favourite of favouriteShows) {
-
-    htmlCodeFavouriteShow += `<li class="list" class="card__show" id="${favourite.id}">`;
-    htmlCodeFavouriteShow += `<h3 class="card__show__title"> ${favourite.name}</h3>`;
-    htmlCodeFavouriteShow += `<img class="card__show__img" src="${favourite.image}">`;
-    htmlCodeFavouriteShow += `</li>`;
-    htmlCodeFavouriteShow += `<button class="js-button js-id-remove"  data-id="${favourite.id}" >Delete favourites</button>`
-
-  }
-  ulFavourite.innerHTML = htmlCodeFavouriteShow;
-  setInLocalStorage();
-  lintenFavouriteShow();
-  detectBtnRemove();
+  return htmlCodeFav;
 }
 
+// funcion para pintar series fav
+function paintCodeFavouriteShow() {
+  let htmlFavShow = '';
+  for (const favourite of favouriteShows) {
+    htmlFavShow += getHtmlCodeFavShow(favourite);
+  }
+  ulFavourite.innerHTML = htmlFavShow;
+  setInLocalStorage();
+  detectBtnRemove();
+}
 // eliminar de la lista de favoritos
 
 function detectBtnRemove() {
@@ -136,36 +144,29 @@ function detectBtnRemove() {
   }
 }
 
-
 function removeFavorite(ev) {
   const clickedDeleted = ev.currentTarget;
   for (let index = 0; index < favouriteShows.length; index++) {
-    if (clickedDeleted === parseInt(favouriteShows[index].id))
-
-      console.log(favouriteShows[index].id);
+    if (clickedDeleted === parseInt(favouriteShows[index].id));
     favouriteShows.splice(index, 1);
   }
 
-  paintFavouritesShows();
+  paintCodeFavouriteShow();
   setInLocalStorage();
-
 }
-
 
 // funcion para guardar favoritas en localstorage
 function getFromLocalStorage() {
   const localStorageShow = localStorage.getItem('favourite');
   if (localStorageShow !== null) {
     favouriteShows = JSON.parse(localStorageShow);
-    paintFavouritesShows();
-
+    paintCodeFavouriteShow();
   }
 }
 
 function setInLocalStorage() {
   const strinfigyShow = JSON.stringify(favouriteShows);
   localStorage.setItem('favourite', strinfigyShow);
-
 }
 // evento click del botón buscar
 
